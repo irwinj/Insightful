1a63db37258a01d73e7928d8e8dd3adb6c0f837e
chibitofu <erinmoony@gmail.com>
2016-01-11 01:45:23 -0800

    Updates to d3.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,31 +35,43 @@ $(document).ready(function(){
         });
 
       rectangles
-      .attr("height", function(d) {
-        console.log(d);
+      .attr("height", function(d, i) {
         return d.percentage * 125;
       })
-      .attr("width", function(d, i ) {
-          return (w / bigFive.length - 3);
-        })
-      .attr("x", function(d , i) {
-        return i * (w / bigFive.length);
+      .attr("width", 0)
+      .attr("x", function(d, i) {
+        return (w / 2);
       })
       .attr("y", function(d, i) {
         return h - (d.percentage * 125);
-      })
-      .attr("fill", function(d) {
-        return "rgb(" + Math.round(255 * d.percentage) + ",0,0)";
       });
 
+      rectangles
+      .transition()
+      .duration(2000)
+      .attr("x", function(d , i) {
+        return (i * (w / bigFive.length));
+      })
+      .attr("width", function(d, i ) {
+          return (w / bigFive.length - 3);
+      })
+      .attr("fill", function(d) {
+          return "rgb(" + Math.round(255 * d.percentage) + ",0,0)";
+        });
 
-      svg.selectAll("text")
+    var titles = svg.selectAll("text")
       .data(bigFive)
       .enter()
       .append("text")
       .text(function(d) {
         return d.name + " " + (d.percentage.toFixed(3)) + "%";
       })
+    .attr("fill", "rgb(255, 255, 255)")
+    .attr("fill-opacity", "0.0");
+
+      titles
+      .transition()
+      .duration(3000)
       .attr("x", function(d, i) {
         return i * (w / bigFive.length) + (w / bigFive.length - 3) / 2;
       })
@@ -67,59 +79,13 @@ $(document).ready(function(){
         return h - (d.percentage * 125) - 5;
       })
       .attr("font-size", "12px")
+      .attr("font-family", "sans-serif")
       .attr("fill", "rgb(0, 0, 0)")
+      .attr("fill-opacity", "1.0")
       .attr("text-anchor", "middle");
-
-
   });
 
-//   var w = 500;
-//   var h = 150;
-//
-//   var svg = d3.select("body")
-//             .append("svg")
-//             .attr("width", w)   // <-- Here
-//             .attr("height", h); // <-- and here!
-//
-//   var rectangles = svg.selectAll("rect")
-//     .data(bigFive.percentage)
-//     .enter()
-//     .append("rect");
-//
-//   rectangles.attr("height", function(d, i) {
-//     return d * 4;
-//   })
-//   .attr("width", function(d, i) {
-//     return w / bigFive.length - 3;
-//   })
-//   .attr("x", function(d, i) {
-//     return i * (w / bigFive.length);
-//   })
-//   .attr("y", function(d, i) {
-//     return h - d * 4;
-//   })
-//   .attr("fill", function(d, i) {
-//     return "rgb(0, 0, " + (d*10) + ")";
-//   });
-//
-//   svg.selectAll("text")
-//     .data(bigFive.percentage)
-//     .enter()
-//     .append("text")
-//     .text(function(d) {
-//       return d;
-//     })
-//     .attr("x", function(d, i) {
-//       return i * (w / bigFive.length) + (w / bigFive.length - 3) / 2;
-//     })
-//     .attr("y", function(d, i) {
-//       return h - (d * 4) + 14;
-//     })
-//     .attr("font-family", "sans-serif")
-//     .attr("font-size", "11px")
-//     .attr("fill", "white")
-//     .attr("text-anchor", "middle");
-// });
-//   var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-//                 11, 12, 15, 20, 18, 17, 16, 18, 23, 25, 25];
+
+
+
 });
